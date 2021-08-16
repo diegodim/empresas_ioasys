4e4510beeb6c2a669b6e5d71eb0b82edb40a07dd
Diego Duarte <diegoduarte@ioasys.com.br>
2021-08-16 16:56:39 -0300

    Implement the enterprise viewModel

--- presentation_main/src/main/java/com/diego/duarte/presentation_main/MainViewModel.kt
+++ presentation_main/src/main/java/com/diego/duarte/presentation_main/MainViewModel.kt
@@ -1,4 +1,33 @@
 package com.diego.duarte.presentation_main
 
-class MainViewModel {
+import androidx.lifecycle.ViewModel
+import com.diego.duarte.base_presentation.utils.extensions.*
+import com.diego.duarte.domain.model.Enterprise
+import com.diego.duarte.domain.usecase.Search
+import org.koin.core.component.KoinComponent
+
+class MainViewModel: ViewModel(), KoinComponent {
+
+    private val search: Search by useCase()
+
+    private val _searchState by viewState<List<Enterprise>>()
+
+    val searchViewState = _searchState.asLiveData()
+
+    fun search(key: String){
+        search(
+            params = Search.Params(
+                key
+            ),
+            onSuccess = {
+                _searchState.postSuccess(it)
+            },
+            onError = {
+                _searchState.postError(it)
+            }
+        )
+    }
+    fun clearState() {
+        _searchState.postNeutral()
+    }
 }
