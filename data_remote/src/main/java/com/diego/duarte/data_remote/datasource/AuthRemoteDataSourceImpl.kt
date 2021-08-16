7c988b231104373311882ebd30695ed858afc7c7
Diego Duarte <diegoduarte@ioasys.com.br>
2021-08-16 14:27:22 -0300

    Create the enterpriseRemoteDataSource

--- data_remote/src/main/java/com/diego/duarte/data_remote/datasource/AuthRemoteDataSourceImpl.kt
+++ data_remote/src/main/java/com/diego/duarte/data_remote/datasource/AuthRemoteDataSourceImpl.kt
@@ -3,18 +3,16 @@ package com.diego.duarte.data_remote.datasource
 import com.diego.duarte.data.AuthRemoteDataSource
 import com.diego.duarte.data_remote.mapper.TokenResponseMapper
 import com.diego.duarte.data_remote.model.TokenResponse
-import com.diego.duarte.data_remote.service.LoginService
-import com.diego.duarte.domain.exeption.CredentialException
+import com.diego.duarte.data_remote.service.AuthService
 import com.diego.duarte.domain.exeption.InvalidCredentialException
-import com.diego.duarte.domain.exeption.MissingParamsException
 import kotlinx.coroutines.flow.flow
 import org.koin.core.component.KoinComponent
 
-class AuthRemoteDataSourceImpl(private val loginService: LoginService): AuthRemoteDataSource,
+class AuthRemoteDataSourceImpl(private val authService: AuthService): AuthRemoteDataSource,
     KoinComponent {
     
     override fun login(email: String, password:String) = flow {
-        val response = loginService.login(email, password)
+        val response = authService.login(email, password)
         if(response.isSuccessful){
             val token = TokenResponse()
             response.headers().let{
--- di/src/main/java/com/diego/duarte/di/dataRemoteModule.kt
+++ di/src/main/java/com/diego/duarte/di/dataRemoteModule.kt
@@ -5,14 +5,14 @@ import com.diego.duarte.data.AuthRemoteDataSource
 import com.diego.duarte.data_remote.core.WebServiceFactory
 import com.diego.duarte.data_remote.datasource.AuthRemoteDataSourceImpl
 import com.diego.duarte.data_remote.service.EnterpriseService
-import com.diego.duarte.data_remote.service.LoginService
+import com.diego.duarte.data_remote.service.AuthService
 import org.koin.dsl.module
 
 
 val dataRemoteModule = module {
 
     single { WebServiceFactory.provideOkHttpClient() }
-    single<LoginService> { WebServiceFactory.createWebService(get()) }
+    single<AuthService> { WebServiceFactory.createWebService(get()) }
     single<EnterpriseService> { WebServiceFactory.createWebService(get(), get()) }
     single<AuthRemoteDataSource> { AuthRemoteDataSourceImpl(get()) }
 
